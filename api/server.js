e8c7474e45c0a5512665e2b9332834f8ce210e2c
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-13 15:13:47 -0400

    finished the crud operation for the accounts db

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
 const server = express();
+const accountsRouter = require('../Accounts/accounts-Router')
 
+//middleware
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
 
 server.get('/', (req,res)=>{
     res.status(200).json({message: 'server up and running'})
